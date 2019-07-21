dd87c857877cb7a0eb91fe07b43fb58149692c16
asus <1687631602@qq.com>
2019-07-21 09:35:06 +0800

    后端demo

--- src/main/java/com/demo/mms/dao/FinalreportMapper.java
+++ src/main/java/com/demo/mms/dao/FinalreportMapper.java
@@ -4,15 +4,15 @@ import com.demo.mms.common.domain.Finalreport;
 import org.apache.ibatis.annotations.Param;
 
 public interface FinalreportMapper {
-    Finalreport returnFinalreport(@Param("studentid") int studentid, @Param("version")int version);
+    Finalreport returnFinalreport(@Param("studentid") int studentid, @Param("version") int version);
 
     Integer selectMaxversion(int studentid);
 
     void insertFinalreport(Finalreport finalreport);
 
-    void insertFinalreportpath(@Param("finalpath")String finalpath,@Param("studentid") String studentid,@Param("version") String version);
+    void insertFinalreportpath(@Param("finalpath") String finalpath, @Param("studentid") String studentid, @Param("version") String version);
 
-    void insertFinalresultpath(@Param("finalpath")String finalpath,@Param("studentid") String studentid,@Param("version") String version);
+    void insertFinalresultpath(@Param("finalpath") String finalpath, @Param("studentid") String studentid, @Param("version") String version);
 
-    void insertFinalotherpath(@Param("finalpath")String finalpath,@Param("studentid") String studentid,@Param("version") String version);
+    void insertFinalotherpath(@Param("finalpath") String finalpath, @Param("studentid") String studentid, @Param("version") String version);
 }
